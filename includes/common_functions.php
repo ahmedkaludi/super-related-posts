8ca12b6a37e3f1687a875bb381045fa421074e70
Sanjeev Kumar <sanjeevsetu@gmail.com>
2022-06-03 15:56:59 +0530

    .

--- includes/common_functions.php
+++ includes/common_functions.php
@@ -748,4 +748,3 @@ function ppl_content_filter_init() {
 
 // watch out that the registration functions are called earlier
 add_action ('init', 'ppl_content_filter_init');
-add_action ('init', 'ppl_post_filter_init');
