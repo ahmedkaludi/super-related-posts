f0a198b53375fd60c0c86347a3db9100033a6b6e
shridhamdeveloper <shridham55@gmail.com>
2022-11-11 11:04:03 +0530

    #7
    Newsletter Subscription added to Super Related Posts

--- super-related-posts.php
+++ super-related-posts.php
@@ -695,7 +695,8 @@ function srpp_get_tag_terms($ID, $utf8) {
 if ( is_admin() ) {
 	require(SRPP_DIR_NAME.'/admin/super-related-posts-admin.php');
 	require(SRPP_DIR_NAME.'/includes/helper-function.php');
-	require_once( SRPP_DIR_NAME . '/includes/newsletter.php' );
+	// require_once( SRPP_DIR_NAME . '/includes/newsletter.php' );
+	require_once( SRPP_DIR_NAME . '/admin/newsletter.php' );
 
 }
 
